8d0f1875e7f6421d26d726e165c0e9d1167126b1
Luiza Magalhaes <luizalmagalhaes@hotmail.com>
2018-08-18 16:24:33 -0300

    crud de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,48 @@
 class PostsController < ApplicationController
   def index
-    @posts = Posts.all
+    @posts = Post.all
   end
 
   def new
   end
 
   def create
-    @post = Post.new(params[:post])
-    @post.save
-    redirect_to @post
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else 
+      render :edit
+    end    
+  end
+
+  def destroy 
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
   private 
 
-  
-  
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
